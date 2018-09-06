1e8da8085e7912e28ab2eb572be842b06066e3ca
Walter Martin <wamartin@microsoft.com>
2018-09-06 13:00:45 -0400

    working bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,18 +2,18 @@ var express = require("express");
 var open = require("open");
 var path = require("path");
 var webpack = require("webpack");
-var webpack_config_dev = require("../webpack.config.dev");
+var config = require("../webpack.config.dev");
 var port = 3000;
 var app = express();
-var compiler = webpack["default"](webpack_config_dev["default"]);
+var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
-    publicPath: webpack_config_dev_1["default"].output.publicPath
+    publicPath: config.output.publicPath
 }));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function (req, res) {
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,59 @@
-import path from 'path';
+var path = require('path');
 
-export default {
-    debug: true,
-    devtool: 'inline-source-map',
-    noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
-    target: 'web', // could be node instead, or electron
-    output: {
-        path: path.resolve(__dirname, 'src'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    plugins: [], // hot reloading, linting, catching errors
-    module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']}, // hopefully change babel out
-            {test: /\.css$/, loaders: ['style', 'css']}
-        ]
-    }
-}
+// module.exports = {
+//     devtool: 'inline-source-map',
+//     entry: [
+//         path.resolve(__dirname, 'src/index.ts') // TODO this probably needs to be changed to work with dist
+//     ],
+//     resolve: {
+//         // Add '.ts' and '.tsx' as resolvable extensions.
+//         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+//     },
+//     target: 'node', // could be node instead, or electron
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         publicPath: '/',
+//         filename: 'bundle.js'
+//     },
+//     plugins: [], // hot reloading, linting, catching errors
+//     module: {
+//         rules: [
+
+//         ]
+//         loaders: [
+//             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+//             {test: /\.css$/, loaders: ['style', 'css']}
+//         ],
+//         preLoaders: [
+//             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+//             { test: /\.js$/, loader: "source-map-loader" }
+//         ]
+//     }
+// }
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src/index.ts') // TODO this probably needs to be changed to work with dist
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
